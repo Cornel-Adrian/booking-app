e0d814cdae56398545c04cab66df99da1ae3aa0f
Cornel-Adrian <co96grig@gmail.com>
2023-05-25 18:21:44 +0300

    Added controller

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -3,9 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chats.service';
 import { MessageSchema } from './schemas/message.schema';
+import { ChatsController } from './chats.controller';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'Message', schema: MessageSchema }])],
+  controllers:[ChatsController],
   providers: [ChatGateway, ChatService],
 })
 export class ChatModule {}
